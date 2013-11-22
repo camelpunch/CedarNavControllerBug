c4f90e139621e1f42560f155decd016aaac3fc6a
Andrew Bruce <pair+abruce@pivotallabs.com>
2013-11-22 08:50:29 -0800

    Initial Commit

---
+++ CedarNavControllerBug/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CedarNavControllerBug
+//
+//  Created by pivotal on 11/22/13.
+//  Copyright (c) 2013 pivotal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CedarNavControllerBug/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CedarNavControllerBug
+//
+//  Created by pivotal on 11/22/13.
+//  Copyright (c) 2013 pivotal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
